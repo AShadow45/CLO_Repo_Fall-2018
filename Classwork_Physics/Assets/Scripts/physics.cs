79a2d5cd78a13fbf5d1e42a569ec3546cae610d3
AShadow45 <guada271@newschool.edu>
2018-09-17 18:27:45 -0400

    Upload classwork and minigame

---
+++ Classwork_Physics/Assets/Scripts/physics.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class physics : MonoBehaviour {
+
+    public float speed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left *speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right *speed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * upforce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.down * upforce);
+        }
+    }
+
+    void OnCollisionEnter (Collision other) {
+        //Debug.Log("I touched something!!");
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Sphere") {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    void OnCollisionStay (Collision other) {
+        Debug.Log("I'm still touching!!");
+        //GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+        if (other.gameObject.tag == "Sphere") {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    void OnCollisionExit (Collision other) {
+        Debug.Log("No longer touching something!!");
+        if (other.gameObject.tag == "Sphere") {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    public float upforce;
+
+     void OnMouseDown() {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+        GetComponent<Rigidbody>().AddTorque(Vector3.up * upforce);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+        GetComponent<Rigidbody>().AddTorque(Vector3.up * upforce);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
---
+++ Classwork_Sept4/Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //these are the same! Forward is shorthand for (0,0,1)
+        //transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+    }
+}
