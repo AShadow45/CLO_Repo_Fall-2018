1b4d4cc1d0125b4ff3db11c5926b62d3be545abf
AShadow45 <guada271@newschool.edu>
2018-10-22 17:03:44 -0400

    Quiz Updates

--- Technical_Quiz_Oct18/Assets/Scripts/CameraFollow.cs
+++ Technical_Quiz_Oct18/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,6 @@ public class CameraFollow : MonoBehaviour {
     public Transform target;
 
     void LateUpdate () {
-        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
     }
 }
--- Technical_Quiz_Oct18/Assets/Scripts/PlayerControl.cs
+++ Technical_Quiz_Oct18/Assets/Scripts/PlayerControl.cs
@@ -6,10 +6,9 @@ public class PlayerControl : MonoBehaviour {
 
     public float force = 300;
     public float speed = 2;
-    public bool isDead = false;
+   
 
-    private Animator anim;
-    private Rigidbody2D rb2d;
+ 
 	
 	void Start () {
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
@@ -17,10 +16,8 @@ public class PlayerControl : MonoBehaviour {
 	
 	
 	void Update () {
-        if (isDead == false) {
-            if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)){
                 GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
-            }
         }
         
 	}
