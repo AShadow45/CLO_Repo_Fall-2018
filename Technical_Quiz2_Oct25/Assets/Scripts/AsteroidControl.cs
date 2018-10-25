8103becf8c65ddb456e8fac71cedb50fc0f29326
AShadow45 <guada271@newschool.edu>
2018-10-25 18:28:38 -0400

    update midterm and technical quiz

---
+++ Technical_Quiz2_Oct25/Assets/Scripts/AsteroidControl.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidControl : MonoBehaviour {
+
+
+    //   public float spawnTime = 3f;
+    //   public Transform[] spawnPoints;
+
+    //   public float waitingForNextRock = 10;
+    //   public float waittime = 10;
+
+       public float xMin;
+       public float xMax;
+
+       public float yMin;
+       public float yMax;
+
+    //   void Start () {
+    //       InvokeRepeating("Spawn", spawnTime, spawnTime);
+    //   }
+
+
+    //void Update () {
+    //       waittime -= Time.deltaTime;
+    //       if (waittime <= 0)
+    //       {
+    //           Spawnspacerocks();
+    //           waittime = waitingForNextRock;
+    //       }
+    //   }
+
+    //   void Spawnspacerocks()
+    //   {
+    //       Vector2 pos = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
+
+
+
+
+    //   }
+
+    //   void Spawn()
+    //   {
+
+    //       // Find a random index between zero and one less than the number of spawn points.
+    //       int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+    //       // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+    //       Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    //   }
+
+    //previous attempts above
+    //-----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    public GameObject spacerock;
+    float timer = 0;
+
+    GameObject[] rocks;
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer >= 5)
+        {
+            GameObject Clone = Instantiate (spacerock, new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax)), Quaternion.identity);
+            //Clone.transform.localScale = new Vector2(Random.Range(2f, 8f), 1f);
+            timer = 0;
+        }
+
+        rocks = GameObject.FindGameObjectsWithTag("Asteroid");
+
+        foreach (GameObject platform in rocks)
+        {
+            spacerock.transform.localPosition = new Vector2(spacerock.transform.localPosition.x, spacerock.transform.localPosition.y - 0.1f);
+
+           // if (spacerock.transform.localPosition.y < -6)
+            //{
+             //   Destroy(spacerock);
+            }
+        }
+
+
+    }
+
+}
---
+++ Technical_Quiz2_Oct25/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed = 5;
+    public object PlayerCamera;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        transform.Rotate(Input.mousePosition.x, Input.mousePosition.y);
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Pressed left click.");
+
+           
+
+            if (Input.GetButton("Fire1"))
+            {
+
+            }
+
+        }
+      
+        // if i wanted the player to be follow mouse [directly under cursor]
+        //target = PlayerCamera.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        //transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+        //  Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+    }
+
+}
