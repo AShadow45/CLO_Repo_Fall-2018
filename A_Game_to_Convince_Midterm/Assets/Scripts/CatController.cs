41b706a38b3ca468061c9793307df61d00749f1a
AShadow45 <guada271@newschool.edu>
2018-10-30 17:46:16 -0400

    update Midterm and class notes

---
+++ A_Game_to_Convince_Midterm/Assets/Scripts/CatController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+    private AudioSource source;
+    public AudioClip catPurr;
+
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+    void Awake() {
+        source = GetComponent<AudioSource>();
+    }
+
+
+    void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        float vol = Random.Range(volLowRange, volHighRange);
+        source.PlayOneShot(catPurr, vol);
+    }
+}
---
+++ A_Game_to_Convince_Midterm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Sprite walk;
+    public Sprite stand;
+    public float moveSpeed = 2f;
+
+    SpriteRenderer sr;
+    //Animator anim;
+
+    void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        //anim = GetComponent<Animator>();
+    }
+	
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            sr.sprite = walk;
+        } else {
+            sr.sprite = stand;
+        }
+	}
+
+   
+}
